0df87676a260e5f93764472e20042e77691c32e1
Giulio Fagioli <fagioli.giulio@gmail.com>
2018-08-25 09:46:50 +0200

    Add copy button, some minor fixes to css

--- src/Mail.js
+++ src/Mail.js
@@ -7,10 +7,18 @@ class Mail extends Component {
     let lengthWord = 4
     this.state = {lengthWord: lengthWord, suffix: '@mailnesia.com',word: gen(lengthWord)}
     this.newMail = this.newMail.bind(this);
+    this.copy = this.copy.bind(this);
   }
 
   newMail() {
     this.setState({word: gen(this.state.lengthWord)})
+  }
+
+  copy() {
+    console.log("prova");
+    const email = document.getElementById("email")
+    email.select()
+    document.execCommand("copy")
   }
 
   render() {
@@ -18,10 +26,11 @@ class Mail extends Component {
       <div>
           <div className="mail__box">
             <div className="mail__input">
-              <p>{this.state.word + this.state.suffix}</p>
+              <input className="email" type="email" id="email" value={this.state.word + this.state.suffix} readOnly />
             </div>
-            <div className="mail__button">
-              <button className="button" onClick={this.newMail}>Genera!</button>
+            <div className="mail__buttons">
+              <button className="button" onClick={this.copy}>Copia 📄</button>
+              <button className="button" onClick={this.newMail}>Genera 🚀</button>
             </div>
           </div>
       </div>
