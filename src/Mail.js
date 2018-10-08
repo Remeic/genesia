84fbcb2b3a77efaf00bcb6f1f06699179b934d94
Yasser Batas <yasser.batas@gmail.com>
2018-10-08 12:26:34 -0500

    add toast message in bottom left
    - has been added a toast message when the email is copied
    - has been replaced binding functions by arrow functions
    - 'prettier' plugin has been applied

--- src/Mail.js
+++ src/Mail.js
@@ -1,42 +1,69 @@
-import React, { Component } from 'react'
-import gen from './wordGenerator'
+import React, { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import gen from './wordGenerator';
 import polyglot from './translator';
 
 class Mail extends Component {
   constructor(props) {
-    super(props)
-    let lengthWord = 4
-    this.state = {lengthWord: lengthWord, suffix: '@mailnesia.com',word: gen(lengthWord)}
-    this.newMail = this.newMail.bind(this);
-    this.copy = this.copy.bind(this);
+    super(props);
+    let lengthWord = 4;
+    this.state = {
+      lengthWord: lengthWord,
+      suffix: '@mailnesia.com',
+      word: gen(lengthWord)
+    };
   }
 
-  newMail() {
-    this.setState({word: gen(this.state.lengthWord)})
-  }
+  notify = () => {
+    toast('The mail has been copied to clipboard.', {
+      position: toast.POSITION.BOTTOM_LEFT
+    });
+  };
 
-  copy() {
-    console.log("prova");
-    const email = document.getElementById("email")
-    email.select()
-    document.execCommand("copy")
-  }
+  newMail = () => {
+    this.setState({ word: gen(this.state.lengthWord) });
+  };
+
+  copy = () => {
+    console.log('prova');
+    const email = document.getElementById('email');
+    email.select();
+    document.execCommand('copy');
+    this.notify();
+  };
 
   render() {
     return (
       <div>
-          <div className="mail__box">
-            <div className="mail__input">
-              <input className="email" type="email" id="email" value={this.state.word + this.state.suffix} readOnly />
-            </div>
-            <div className="mail__buttons">
-              <button className="button" onClick={this.copy}>{polyglot.t("copy")} <span role="img" aria-label="sheet">📄</span></button>
-              <button className="button" onClick={this.newMail}>{polyglot.t("generate")} <span role="img" aria-label="rocket">🚀</span></button>
-            </div>
+        <div className="mail__box">
+          <div className="mail__input">
+            <input
+              className="email"
+              type="email"
+              id="email"
+              value={this.state.word + this.state.suffix}
+              readOnly
+            />
           </div>
+          <div className="mail__buttons">
+            <button className="button" onClick={this.copy}>
+              {polyglot.t('copy')}{' '}
+              <span role="img" aria-label="sheet">
+                📄
+              </span>
+            </button>
+            <button className="button" onClick={this.newMail}>
+              {polyglot.t('generate')}{' '}
+              <span role="img" aria-label="rocket">
+                🚀
+              </span>
+            </button>
+          </div>
+        </div>
+        <ToastContainer autoClose={4000} />
       </div>
-    )
+    );
   }
 }
 
-export default Mail
+export default Mail;
