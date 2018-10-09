3ab7c867c02321ce5ed980b6d33e36b681e0c3bd
pkoushik <pkoushik@akamai.com>
2018-10-09 11:58:18 +0530

    Fixes #4

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import Mail from './Mail'
 
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1 className="title">Genesia</h1>
-        <Mail></Mail>
-        <h4 className="thanks" >Thanks to <a href="http://mailnesia.com/">Mailnesia</a></h4>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1 className="title">Genesia</h1>
+                <Mail></Mail>
+                <h4 className="thanks" >Thanks to <a href="http://mailnesia.com/">Mailnesia</a></h4>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Mail.js
+++ src/Mail.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react'
 import gen from './wordGenerator'
 import polyglot from './translator';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 
 class Mail extends Component {
   constructor(props) {
     super(props)
     let lengthWord = 4
-    this.state = {lengthWord: lengthWord, suffix: '@mailnesia.com',word: gen(lengthWord)}
+    this.state = { lengthWord: lengthWord, suffix: '@mailnesia.com', word: gen(lengthWord) }
     this.newMail = this.newMail.bind(this);
     this.copy = this.copy.bind(this);
   }
 
   newMail() {
-    this.setState({word: gen(this.state.lengthWord)})
+    this.setState({ word: gen(this.state.lengthWord) })
   }
 
   copy() {
@@ -25,15 +28,22 @@ class Mail extends Component {
   render() {
     return (
       <div>
-          <div className="mail__box">
-            <div className="mail__input">
-              <input className="email" type="email" id="email" value={this.state.word + this.state.suffix} readOnly />
-            </div>
-            <div className="mail__buttons">
-              <button className="button" onClick={this.copy}>{polyglot.t("copy")} <span role="img" aria-label="sheet">📄</span></button>
-              <button className="button" onClick={this.newMail}>{polyglot.t("generate")} <span role="img" aria-label="rocket">🚀</span></button>
-            </div>
+        <div className="mail__box">
+          <div className="mail__input">
+            <TextField className="email" type="email"
+              name="email"
+              autoComplete="email"
+              margin="normal"
+              id="email"
+              value={this.state.word + this.state.suffix}
+              readOnly
+              margin="normal" />
           </div>
+          <div className="mail__buttons">
+            <Button variant="contained" className="button" onClick={this.copy}>{polyglot.t("copy")} <span role="img" aria-label="sheet">📄</span></Button>
+            <Button variant="contained" className="button" onClick={this.newMail}>{polyglot.t("generate")} <span role="img" aria-label="rocket">🚀</span></Button>
+          </div>
+        </div>
       </div>
     )
   }
