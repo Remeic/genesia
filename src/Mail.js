b1b359795806f19226efd90c644f1abeb0228088
Costi <mail.la.costi@gmail.com>
2018-10-31 16:49:39 +0200

    rewrote  Mail.js using hooks

--- src/Mail.js
+++ src/Mail.js
@@ -1,69 +1,63 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import gen from './wordGenerator';
 import polyglot from './translator';
 
-class Mail extends Component {
-  constructor(props) {
-    super(props);
-    let lengthWord = 4;
-    this.state = {
-      lengthWord: lengthWord,
-      suffix: '@mailnesia.com',
-      word: gen(lengthWord)
-    };
-  }
+let defaultLengthWord = 4;
 
-  notify = () => {
+function Mail() {
+  const [lengthWord] = useState(defaultLengthWord);
+  const [suffix] = useState('@mailnesia.com');
+  const [word, setWord] = useState(gen(defaultLengthWord));
+
+  function notify() {
     toast(polyglot.t('copied'), {
       position: toast.POSITION.BOTTOM_LEFT
     });
   };
 
-  newMail = () => {
-    this.setState({ word: gen(this.state.lengthWord) });
+  function newMail() {
+    setWord(gen(lengthWord));
   };
 
-  copy = () => {
-    console.log('prova');
+  function copy() {
     const email = document.getElementById('email');
     email.select();
     document.execCommand('copy');
-    this.notify();
+    notify();
   };
 
-  render() {
-    return (
-      <div>
-        <div className="mail__box">
-          <div className="mail__input">
-            <input
-              className="email"
-              type="email"
-              id="email"
-              value={this.state.word + this.state.suffix}
-              readOnly
-            />
-          </div>
-          <div className="mail__buttons">
-            <button className="button" onClick={this.copy}>
-              {polyglot.t('copy')}{' '}
-              <span role="img" aria-label="sheet">
-                📄
-              </span>
-            </button>
-            <button className="button" onClick={this.newMail}>
-              {polyglot.t('generate')}{' '}
-              <span role="img" aria-label="rocket">
-                🚀
-              </span>
-            </button>
-          </div>
+  return (
+    <div>
+      <div className="mail__box">
+        <div className="mail__input">
+          <input
+            className="email"
+            type="email"
+            id="email"
+            value={`${word}${suffix}`}
+            readOnly
+          />
         </div>
-        <ToastContainer autoClose={4000} />
+        <div className="mail__buttons">
+          <button className="button" onClick={() => copy()}>
+            {polyglot.t('copy')}{' '}
+            <span role="img" aria-label="sheet">
+              📄
+            </span>
+          </button>
+          <button className="button" onClick={() => newMail()}>
+            {polyglot.t('generate')}{' '}
+            <span role="img" aria-label="rocket">
+              🚀
+            </span>
+          </button>
+        </div>
       </div>
-    );
-  }
+      <ToastContainer autoClose={4000} />
+    </div>
+  );
+
 }
 
 export default Mail;
